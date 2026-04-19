290cf87a14e1f5afe1389841173b3a260460d172
Tal-01 <rose.petal.thorn@tutamail.com>
2026-04-19 12:38:16 -0600

    Transfered FreeRoam to it's own repository

---
+++ Core.cs
@@ -0,0 +1,28 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using UnityEngine;
+using Il2CppQuantum_Game;
+using Il2CppQuantum;
+
+[assembly: MelonInfo(typeof(FreeRoam.Core), "FreeRoam", "1.0.0", "taldo", null)]
+[assembly: MelonGame("Videocult", "Airframe")]
+
+namespace FreeRoam
+{
+    public class Core : MelonMod
+    {
+        [HarmonyPatch(typeof(PlayAreaSystem), "OOBKill", new Type[] {typeof(Frame), typeof(EntityRef)})]
+        private class DisableOOB
+        {
+            public static bool Prefix()
+            {
+                return false;
+            }
+        }
+        
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Goodbye OOB!");
+        }
+    }
+}
